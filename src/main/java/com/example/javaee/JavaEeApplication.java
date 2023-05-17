dd001a80963bab651cef5ab91714f9383773199d
chenhuahong <chenhuahong96@136.com>
2023-05-17 11:03:34 +0800

    init

--- src/main/java/com/example/javaee/JavaEeApplication.java
+++ src/main/java/com/example/javaee/JavaEeApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaEeApplication {
-
+    /**
+     * good
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(JavaEeApplication.class, args);
     }
