a71709ed67806081a2a38361f9f1d69c0dab858e
chenhuahong <chenhuahong96@136.com>
2023-05-17 10:59:32 +0800

    init

---
+++ src/main/java/com/example/javaee/JavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaee/JavaEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
